b5de45eb6ffac2c09d3c9e2f85ebc96ddf95e3a1
Julien Couignoux <j.couignoux@gmail.com>
2022-03-21 17:28:19 +0100

    update is_already_played

--- controllers/tournament.py
+++ controllers/tournament.py
@@ -1,104 +1,172 @@
 from typing import List
 from datetime import datetime
-import json
 
 from models.tournament import Tournament, Round, Match
 from .player import *
 
 
-def get_tournament(self):
+def get_tournament(self, tournaments):
     res = self.view.prompt_for_tournament(
-        self.tournaments, self.menu.tournament_menu())
+        self.menu.tournament_menu(), tournaments)
     if res[0] == "New":
-        get_new_tournament(self)
+        adding_player = True
+        get_new_tournament(self, adding_player)
     elif res[0] == "Ret":
         self.run()
     elif res[0] == "View":
-        self.tournament = res[1]
-        get_tournament_detail(self)
+        self.tournament = tournaments[res[1]]
+        get_tournament_detail(self, self.tournament)
 
 
-def get_tournament_detail(self):
+def get_tournament_detail(self, tournament):
     res = self.view.prompt_for_tournament_detail(
-        self.menu.tournament_detail_menu(), self.tournament)
+        self.menu.tournament_detail_menu(), tournament)
     if res[0] == "Ret":
-        get_tournament(self)
+        get_tournament(self, self.tournaments)
     elif res[0] == "Mod":
-        match = res[1]
-        get_match_detail(self, match)
+        match_index = res[2]
+        get_match_detail(self, tournament, match_index)
     elif res[0] == "Val":
-        add_round(self)
+        end_date = datetime.now().strftime("%d/%m/%Y-%H:%M")
+        tournament.rounds[-1].end_date = end_date
+        add_round(tournament)
+        get_tournament_detail(self, tournament)
+    elif res[0] == "New":
+        add_round(tournament)
+        get_tournament_detail(self, tournament)
+    elif res[0] == "Close":
+        pass
+        # self.db.update_tournament
 
 
-def get_new_tournament(self):
-    date = datetime.now().strftime("%d/%m/%Y")
-    res_t = self.view.prompt_for_new_tournament(
-        self.menu.create_tournament_menu())
-    if res_t[0] == "Abort":
-        get_tournament(self)
-    elif res_t[0] == "Create":
-        self.tournament = Tournament(
-            res_t[1][0], res_t[1][1], res_t[1][2], date, res_t[1][3], res_t[1][4])
-        while True:
-            res_p = add_players(self)
-
-
-def add_players(self):
-    res = self.view.prompt_for_add_player(
-        self.menu.add_player_menu(), self.players)
-    if res[0] == "Stop":
-        get_tournament(self)
-    elif res[0] == "Add":
-        self.tournament.players.append(res[1])
-
-
-def add_round(self):
-    start_date = datetime.now().strftime("%d/%m/%Y-%H:%M")
-    if len(self.tournament.roundst) == 0:
-        round_number = 1
-        self.tournament.rounds.append(round_number, start_date, '')
-        players_list_sorted = sorted(
-            self.tournament.players, key=lambda k: k.ranking)
-        for i in range(len(players_list_sorted) // 2):
-            player1 = (players_list_sorted[i], 0, 0)
-            player2 = (players_list_sorted[i] +
-                       (len(players_list_sorted) // 2), 0, 0)
-            match = Match(player1, player2)
-            self.tournament.rounds.matchs.append(match)
-            match_json = ([json.loads(players_list_sorted[i].__dict__), 0, 0],
-                          [json.loads(players_list_sorted[i + (len(players_list_sorted) // 2)].__dict__), 0, 0])
-            # matchs_json.append(match_json)
+def get_new_tournament(self, adding_player):
+    if adding_player == True:
+        date = datetime.now().strftime("%d/%m/%Y")
+        message = ''
+        res_t = self.view.prompt_for_new_tournament(
+            self.menu.create_tournament_menu())
+        if res_t[0] == "Abort":
+            get_tournament(self)
+        elif res_t[0] == "Create":
+            self.tournament = Tournament(
+                res_t[1][0], res_t[1][1], date, res_t[1][2], res_t[1][3], res_t[1][4])
+            while adding_player == True:
+                add_players(self, self.tournament, message)
     else:
-        round_number = len(self.tournament.rounds) + 1
-        player_list_temp = []
-        for match in self.tournament.rounds.matchs:
-            for player in match:
-                player_list_temp.append([player[0], player[1] + player[2], 0])
+        add_round(self.tournament)
+        self.tournaments.append(self.tournament)
+        get_tournament_detail(self, self.tournament)
 
-        players_list_sorted = sorted(
-            player_list_temp, key=lambda k: (k[1], k[0].ranking))
 
+def add_players(self, tournament, message):
+    res = self.view.prompt_for_add_player(
+        self.menu.add_player_menu(), tournament, self.players, message)
+    if res[0] == "Stop":
+        if (len(tournament.players) % 2) == 0:
+            adding_player = False
+            get_new_tournament(self, adding_player)
+        else:
+            message = "Il doit y avoir un nombre de joueurs paire"
+    elif res[0] == "Add":
+        player = res[1]
+        tournament.add_player(player)
+    elif res[0] == "Create":
+        player = create_player(self)
+        # print(player)
+        # input('')
+        tournament.players.append(player)
+
+
+def add_round(tournament):
+    start_date = datetime.now().strftime("%d/%m/%Y-%H:%M")
+    round_number = len(tournament.rounds) + 1
     round = Round(round_number, start_date, '')
-    round_json = {'round_number': round_number,
-                  'start_date': start_date}
-    self.tournament.rounds.append(round)
-    # tournament_json['rounds_list'].append(round_json)
-    # for round in tournament.rounds_list:
-    #     rounds_list_json.append(json.dumps(round.__dict__))
-    # print(tournament_json['rounds_list'])
-    # table.update({'rounds_list': tournament_json['rounds_list']},
-    #  where('name') == tournament.name)
+    if len(tournament.rounds) == 0:
+        players_list_sorted = sorted(
+            tournament.players, key=lambda k: k.ranking)
+        for i in range(len(players_list_sorted) // 2):
+            player1 = players_list_sorted[i], 0, 0
+            player2 = players_list_sorted[i +
+                                          (len(players_list_sorted) // 2)], 0, 0
+            match = Match()
+            match.player1 = player1
+            match.player2 = player2
+            round.matchs.append(match)
+    else:
+        # créer une liste : à faire en méthode
+        matchs_played_list = []
+        for r in tournament.rounds:
+            for m in r.matchs:
+                p = m.player1[0].last_name, m.player2[0].last_name
+                p_inv = m.player2[0].last_name, m.player1[0].last_name
+                matchs_played_list.append(p)
+                matchs_played_list.append(p_inv)
+        player_list_temp = []
+        for match in tournament.rounds[-1].matchs:
+            player_list_temp.append(
+                [match.player1[0], match.player1[1] + match.player1[2], 0])
+            player_list_temp.append(
+                [match.player2[0], match.player2[1] + match.player2[2], 0])
+        players_list_sorted = sorted(
+            player_list_temp, key=lambda k: (-k[1], k[0].ranking))
+        # ref list
+        player_list_pop = list(tournament.players)
+        # first player index
+        i1 = 0
+        # second player index
+        i2 = 1
+        for item in range(len(players_list_sorted) // 2):
+            # Initialize number of matchs
+            for n in range(len(player_list_pop) - 1):
+                player1 = [players_list_sorted[i1][0],
+                           players_list_sorted[i1][1], 0]
+                # Test if first ans second players in ref list
+                if players_list_sorted[i1][0] in player_list_pop:
+                    if players_list_sorted[i2][0] in player_list_pop:
+                        # Check if match already played
+                        couple = (
+                            players_list_sorted[i1][0].last_name,
+                            players_list_sorted[i2][0].last_name
+                        )
+                        if not tournament.is_already_played(couple):
+                            if players_list_sorted[i2][0] in player_list_pop:
+                                player2 = [players_list_sorted[i2][0],
+                                           players_list_sorted[i2][1], 0]
+                                # remove player from ref list
+                                player_list_pop.remove(
+                                    players_list_sorted[i1][0])
+                                player_list_pop.remove(
+                                    players_list_sorted[i2][0])
+                                match = Match()
+                                match.player1 = player1
+                                match.player2 = player2
+                                round.add_match(match)
+                                i1 += 1
+                                i2 = i1 + 1
+                        else:
+                            print(str(players_list_sorted[i1][0].last_name) + " et " +
+                                  str(players_list_sorted[i2][0].last_name) + str(" ont déjà joué ensemble."))
+                            input('')
+                            i2 += 1
+                    else:
+                        # print("p2 pas dans player_pop")
+                        i2 += 1
+                else:
+                    print("p1 pas dans player_pop")
+                    i1 += 1
+                    i2 = i1 + 1
+        # input('fin')
+    # tournament.rounds.append(round)
+    tournament.add_round(round)
 
-    return self
 
-
-def get_match_detail(self, match):
+def get_match_detail(self, tournament, match_index):
     res = self.view.prompt_for_match_detail(
-        self.menu.match_detail_menu(), match)
+        self.menu.match_detail_menu(), tournament, match_index)
     if res[0] == "Mod":
+        match = tournament.rounds[-1].matchs[match_index]
         match.player1 = (match.player1[0], match.player1[1], res[1][0])
         match.player2 = (match.player2[0], match.player2[1], res[1][1])
-        # self.db.update_match(self.tournament, match)
-        get_tournament_detail(self)
+        get_tournament_detail(self, tournament)
     if res[0] == "Ret":
-        get_tournament_detail(self)
+        get_tournament_detail(self, tournament)
--- models/tournament.py
+++ models/tournament.py
@@ -13,13 +13,13 @@ class Match:
         self.player1: Player = Player, float(), float()
         self.player2: Player = Player, float(), float()
 
-
-class Matchs(list):
-
-    def append(self, object):
-        if not isinstance(object, Match):
-            return ValueError("Vous ne pouvez ajouter que des matchs !")
-        return super().append(object)
+    def is_already_played(self, couple):
+        test = [
+            (self.player1[0].last_name, self.player2[0].last_name),
+            (self.player2[0].last_name, self.player1[0].last_name)
+        ]
+        if couple in test:
+            return True
 
 
 class Round:
@@ -28,65 +28,46 @@ class Round:
         self.number = number
         self.start_date = start_date
         self.end_date = end_date
-        self.matchs: List[Match] = Matchs()
+        self.matchs = []
 
-    # def match(self):
-    #     matchs_list = []
-    #     for match in self.matchs_list:
-    #         p1_j = json.loads(match[0][0])
-    #         player1 = Player(p1_j['last_name'], p1_j['for_name'],
-    #                          p1_j['birthday'], p1_j['gender'], p1_j['ranking'])
-    #         total1 = int(match[0][1])
-    #         score1 = int(match[0][2])
-    #         p2_j = json.loads(match[1][0])
-    #         player2 = Player(p2_j['last_name'], p2_j['for_name'],
-    #                          p2_j['birthday'], p2_j['gender'], p2_j['ranking'])
-    #         total2 = int(match[1][1])
-    #         score2 = int(match[1][2])
-    #         matchs_list.append(
-    #             Match(player1, total1, score1, player2, total2, score2))
+    def add_match(self, match):
+        if not isinstance(match, Match):
+            return ValueError("Vous ne pouvez ajouter que des matchs !")
+        self.matchs.append(match)
 
-    #     return matchs_list
-
-
-class Rounds(list):
-
-    def append(self, object):
-        if not isinstance(object, Round):
-            return ValueError("Vous ne pouvez ajouter que des tours !")
-        return super().append(object)
-
-    def list(self, object):
-        if not isinstance(object, Round):
-            return ValueError("Vous ne po")
-        return super()
-
-
-class Players(list):
-
-    def append(self, object):
-        if not isinstance(object, Player):
-            return ValueError("Vous ne pouvez ajouter que des joueurs !")
-        return super().append(object)
+    def is_already_played_round(self, couple):
+        for match in self.matchs:
+            if match.is_already_played(couple):
+                return True
 
 
 class Tournament:
 
-    def __init__(self, name, description, date, location, round_number, time_controler):
+    def __init__(self, name, description, date, location, round_number, time_controler):  # , rounds, players
         self.name = name
         self.description = description
         self.date = date
         self.location = location
         self.round_number = int(round_number)
-        self.rounds: List[Round] = Rounds()
-        self.players: List[Player] = Players()
+        self.rounds = []
+        self.players = []
         self.time_controler = time_controler
 
-    def serialized_tournament(self):
-        self.tournament = table.all()
+    def add_player(self, player):
+        if not isinstance(player, Player):
+            return ValueError("Vous ne pouvez ajouter que des tours !")
+        self.players.append(player)
 
-    def add_tournament(self):
-        pass
+    def add_round(self, round):
+        if not isinstance(round, Round):
+            return ValueError("Vous ne pouvez ajouter que des tours !")
+        self.rounds.append(round)
+
+    def is_already_played(self, couple):
+        for round in self.rounds:
+            if round.is_already_played_round(couple):
+                return True
+        # return True
 
     def __str__(self):
         """Used in print."""
@@ -95,22 +76,3 @@ class Tournament:
     def __repr__(self):
         """Used in print."""
         return str(self)
-    # def get_players(self):
-    #     players_list = []
-    #     for player_json in self.players_list:
-    #         p_j = json.loads(player_json)
-    #         player = Player(p_j['last_name'], p_j['for_name'],
-    #                         p_j['birthday'], p_j['gender'], p_j['ranking'])
-    #         players_list.append(player)
-
-    #     return players_list
-
-    # def get_rounds(self):
-    #     rounds_list = []
-    #     for round_json in self.rounds_list:
-    #         r_j = round_json
-    #         round = Round(r_j['number'], r_j['start_date'], r_j['end_date'],
-    #                       r_j['matchs_list'])
-    #         rounds_list.append(round)
-
-    #     return rounds_list
